4e92d93098d2e3bdbeb8ee0f14f9e176ad623ec9
Kojo <yalleyabraham@yahoo.com>
2023-08-12 16:57:43 +0800

    n

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:learn_flutter/login_page.dart';
 
 void main() {
   runApp(MyAPP());
@@ -8,32 +9,7 @@ class MyAPP extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-          backgroundColor: Colors.blue[900],
-          
-            body: Column(
-              children: [
-                Text("Hello, welcome back"),
-                 Text('Login to continue'),
-                 TextField(
-                  decoration: InputDecoration(
-                    hintText: 'Username'
-                  ),
-                 ),
-                 TextField(
-                  decoration: InputDecoration(
-                    hintText: 'Password'
-                  ),
-                 ),
-                 TextButton(onPressed:(){
-
-                 }, child: Text('Forgot password?')),
-                 ElevatedButton(onPressed:(){
-
-                 }, 
-                 child: Text('Log in')),
-                 Text('Or sign in with')
-            
-          ])));
+      home: LoginPage()
+    );
   }
 }
